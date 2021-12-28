01800da93454669f5cda2799912cf9d954257242
thaiis-souto7 <dammascenothais@gmail.com>
2021-12-28 17:53:10 -0300

    Aula 15

---
+++ Atividades/exercicios/exercicio-06/validar.php
@@ -0,0 +1,24 @@
+<?php
+
+    // echo "POST";
+    // var_dump($_POST);
+
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $cep = $_POST['cep'];
+
+
+    //Controller
+    if ($nome != "" && $cpf != "" && $email != "" && $telefone != "" && $cep != ""){
+        echo "Nome: $nome </br>";
+        echo "CPF: $cpf </br>";
+        echo "Email: $email </br>";
+        echo "Telefone: $telefone </br>";
+        echo "CEP: $cep </br>";
+    } else {
+        echo "Inválido!";
+    }
+
+    echo '<a href="pessoas.html"></br>Voltar</a>';
