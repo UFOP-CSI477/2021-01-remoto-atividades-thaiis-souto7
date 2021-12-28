26a8fd35ad79687ce800f60a04851069cd62ad2c
thaiis-souto7 <dammascenothais@gmail.com>
2021-12-28 17:30:44 -0300

    Aula 14

---
+++ Atividades/exercicios/exercicio-05/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introdução ao PHP</title>
+</head>
+<body>
+    
+    <?php
+        $user = "Thais";
+        echo "Meu nome é $user, tenho 22 anos. Curso Sistemas de Informação na Universidade </br>
+        Federal de Ouro Preto, no campus de Ciências Exatas e Aplicadas. Sou nascida na cidade </br>
+        de Belo Horizonte e resido na cidade de João Monlevade, onse se situa a Universidade.";
+    ?>
+
+</body>
+</html>
