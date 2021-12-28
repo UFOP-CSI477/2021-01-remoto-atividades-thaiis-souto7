d7ea323df6b1d13a5ca313391e7286a04d200131
thaiis-souto7 <dammascenothais@gmail.com>
2021-12-28 14:33:10 -0300

    Aula 10 (classe)

--- Atividades/exercicios/exercicio-04/js/perfil.js
+++ Atividades/exercicios/exercicio-04/js/perfil.js
@@ -1,14 +1,24 @@
+function User(name, username, email, tel, cep){
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.tel = tel;
+    this.cep = cep;
+}
+
 function exibeDados(){
 
     const name = window.prompt('Nome: ')
-    const user = window.prompt('Usuário: ')
+    const username = window.prompt('Usuário: ')
     const email = window.prompt('Email: ')
     const tel = window.prompt('Telefone: ')
     const cep = window.prompt('CEP: ')
 
-    document.getElementById("name").innerHTML = name;
-    document.getElementById("user").innerHTML = user;
-    document.getElementById("email").innerHTML = email;
-    document.getElementById("telefone").innerHTML = tel;
-    document.getElementById("cep").innerHTML = cep;
+    const user = new User(name, username, email, tel, cep)
+
+    document.getElementById("name").innerHTML = user.name;
+    document.getElementById("user").innerHTML = user.username;
+    document.getElementById("email").innerHTML = user.email;
+    document.getElementById("telefone").innerHTML = user.tel;
+    document.getElementById("cep").innerHTML = user.cep;
 }
