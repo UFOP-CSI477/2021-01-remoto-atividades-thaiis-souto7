326f9539cd10df88250a14dd1c73eb831c959375
thaiis-souto7 <dammascenothais@gmail.com>
2021-12-29 17:46:56 -0300

    Aula 21

---
+++ Atividades/exercicios/laravel/sistema/database/factories/ProdutoFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProdutoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nome' => $this->faker->lexify('????????????????????'),
+            'um' => $this->faker->lexify('???')
+        ];
+    }
+}
